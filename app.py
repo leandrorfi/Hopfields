d9bebd535907f673a9efbbfb29badd6a349cf740
Leandro Ruiz <leandroruizfariaiqueda@hotmail.com>
2023-09-13 15:57:49 -0300

    Prints para matriz inicial

--- app.py
+++ app.py
@@ -26,6 +26,12 @@ def calcular_pesos_hopfield(matrizes):
 # Cria uma lista com as matrizes A, B e C.
 matrizes = [A, B, C]
 
+# Imprime as matrizes iniciais
+print("Matriz inicial:")
+print(A)
+print(B)
+print(C)
+
 # Calcula os pesos da Rede Neural Hopfield chamando a função 'calcular_pesos_hopfield' e armazena em 'pesos_hopfield'.
 pesos_hopfield = calcular_pesos_hopfield(matrizes)
 
